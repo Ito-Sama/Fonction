f5f9e8e6fcf8fdb8ea2778a56982acc7093dbf8b
Ito-Sama <91988234+Ito-Sama@users.noreply.github.com>
2021-10-05 22:11:35 +0200

    Add files via upload

---
+++ fonction/mainfonc.py
@@ -0,0 +1,9 @@
+print("si tu veux faire une fonction avec des images en negatif")
+print("ecrivez 'nega'. Sinon, ecrivez 'no_nega' et vous pourez faire des fonction plus complexe")
+
+choix = input(">")
+
+if choix == "nega":
+    import fonc_nega_on_no_division_0 
+elif choix == "no_nega":
+    import fonc_nega_off_whith_division_0 
